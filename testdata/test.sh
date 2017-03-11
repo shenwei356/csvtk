eae1eab9db3ff775bd467164752c2934fa2be7cf
shenwei356 <shenwei356@gmail.com>
2017-03-11 14:31:59 +0800

    edit tests

--- testdata/test.sh
+++ testdata/test.sh
@@ -6,71 +6,95 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 set -e
 
 
-cd csvtk; go build; cd ..;
+# cd csvtk; go build; cd ..;
 app=./csvtk/csvtk
 
 set +e
 
 
 # ----------------------------------------------------------------------------
+# test data
+# ----------------------------------------------------------------------------
+
+# $1, rows
+# $2, cols
+# $3, delimiter
+# $4, has header row
+headers() {
+    seq $2 | awk '{print "c"$1}' | paste -sd $3
+}
+matrix() {
+    if [ "$4" = true ]; then
+        headers $1 $2 $3
+    fi
+    for i in $(seq $1); do
+        seq $(( $2*($i-1)+1 )) $(( $2*$i )) | paste -sd $3
+    done
+}
+
+N="1 10"
+
+# ----------------------------------------------------------------------------
 # csvtk headers
 # ----------------------------------------------------------------------------
 
-for n in 1 10 100 10000 1000000; do
-    fn() {
-        cat <(seq $n | awk '{print "c"$1}' | paste -sd,) <(seq $n | paste -sd,) \
-            | $app headers
-    }
-    run "headers (n=$n)" fn
-    assert_no_stderr
-    assert_equal $(cat $STDOUT_FILE | grep -v '#' | wc -l) $n
+for n in $N; do                 # scales
+    for d in "," "\t"; do       # delimiters
+        for h in true false; do # headerrow
+            headrow=""
+            if [ $h = false ]; then
+                headrow=-H
+            fi
+            tab=""
+            if [ $d == "\t" ]; then
+                tab=-t
+            fi
+            fn() {
+                matrix $(($n*2)) $n $d $h | $app $tab $headrow headers 
+            }
+            run "headers $tab $headrow (n=$n)" fn
+            
+            if [ $h = true ]; then
+                assert_no_stderr
+            else
+                assert_in_stderr "flag -H (--no-header-row) ignored"
+            fi
+            assert_equal $(cat $STDOUT_FILE | grep -v '#' | wc -l) $n
+        done
+    done
 done
+
 
 # ----------------------------------------------------------------------------
 # csvtk stats
 # ----------------------------------------------------------------------------
 
-for n in 1 10 100 10000 1000000; do
-    fn() {
-        cat <(seq $n | awk '{print "c"$1}' | paste -sd,) <(seq $n | paste -sd,) \
-            | $app stats
-    }
-    run "stats (n=$n)" fn
-    assert_no_stderr
-    assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 2 | sed 's/,//g') $n
-    assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 3 | sed 's/,//g') 1
-
-    fn() {
-        cat <(seq $n | awk '{print "c"$1}' | paste -sd,) <(seq $n | paste -sd,) \
-            | $app stats -H
-    }
-    run "stats -H (n=$n)" fn
-    assert_no_stderr
-    assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 2 | sed 's/,//g') $n
-    assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 3 | sed 's/,//g') 2
-
-    # -----------------------------------------------------------------------
-    # transpose
-    # -----------------------------------------------------------------------
-    fn() {
-        cat <(seq $n | awk '{print "c"$1}' | paste -sd,) <(seq $n | paste -sd,) \
-            | $app transpose \
-            | $app stats
-    }
-    run "transpose & stats (n=$n)" fn
-    assert_no_stderr
-    assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 2 | sed 's/,//g') 2
-    assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 3 | sed 's/,//g') $(($n -1))
-
-    fn() {
-        cat <(seq $n | awk '{print "c"$1}' | paste -sd,) <(seq $n | paste -sd,) \
-            | $app transpose \
-            | $app stats -H
-    }
-    run "transpose & stats -H (n=$n)" fn
-    assert_no_stderr
-    assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 2 | sed 's/,//g') 2
-    assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 3 | sed 's/,//g') $n
+for n in $N; do                 # scales
+    for d in "," "\t"; do       # delimiters
+        for h in true false; do # headerrow
+            headrow=""
+            if [ $h = false ]; then
+                headrow=-H
+            fi
+            tab=""
+            if [ $d == "\t" ]; then
+                tab=-t
+            fi
+            fn() {
+                matrix $(($n*2)) $n $d $h | $app $tab $headrow stats
+            }
+            run "stats $tab $headrow (n=$n)" fn
+            
+            if [ $h = true ]; then
+                assert_no_stderr
+            else
+                assert_no_stderr
+            fi
+            
+            assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 2 | sed 's/,//g') $n
+            assert_equal $(cat $STDOUT_FILE | $app space2tab | sed 1d | cut -f 3 | sed 's/,//g') $(($n*2))
+        done
+    done
 done
 
 
@@ -78,25 +102,48 @@ done
 # csvtk cut
 # ----------------------------------------------------------------------------
 
-for n in 1 10 100 10000 1000000; do
-    fn() {
-        seq $n | $app cut -H -f 1
-    }
-    run "cut -H -f 1 (n=$n)"  fn
-    assert_no_stderr
-    assert_equal $(cat $STDOUT_FILE | wc -l) $n
-
-    fn() {
-        seq $n | $app cut -H -t -f 1
-    }
-    run "cut -H -t -f 1 (n=$n)"  fn
-    assert_no_stderr
-    assert_equal $(cat $STDOUT_FILE | wc -l) $n
-
-    fn() {
-        cat <(echo head) <(seq $n) | $app cut -f head
-    }
-    run "cut -f head (n=$n)"  fn
-    assert_no_stderr
-    assert_equal $(cat $STDOUT_FILE | wc -l) $(($n+1))
+for n in $N; do                 # scales
+    for d in "," "\t"; do       # delimiters
+        for h in true false; do # headerrow
+            for c in 3 5 4,6; do
+                headrow=""
+                if [ $h = false ]; then
+                    headrow=-H
+                fi
+                tab=""
+                if [ $d == "\t" ]; then
+                    tab=-t
+                fi
+                fn() {
+                    matrix $(($n*2)) $n $d $h | $app $tab $headrow cut -f $c
+                }
+                run "cut -f $c $tab $headrow (n=$n)" fn
+                
+                if [ $n -lt $(echo $c | sed -e 's/,/\t/g' | cut -f 1) ]; then
+                    assert_in_stderr "out of range"
+                    continue
+                else
+                    assert_no_stderr
+                fi
+                
+                if [ $d == "\t" ]; then
+                    assert_equal $(cat $STDOUT_FILE | md5sum | cut -d " " -f 1) $(matrix $(($n*2)) $n $d $h | cut -f $c | md5sum | cut -d " " -f 1)
+                else
+                    assert_equal $(cat $STDOUT_FILE | md5sum | cut -d " " -f 1) $(matrix $(($n*2)) $n $d $h | cut -d, -f $c | md5sum | cut -d " " -f 1)
+                fi
+            done
+        done
+    done
 done
+
+fn() {
+    cat testdata/names.csv | $app cut -f id
+}
+run "cat testdata/names.csv | $app cut -f id" fn
+assert_no_stderr
+assert_equal $(cat $STDOUT_FILE | md5sum | cut -d " " -f 1) $(cat testdata/names.csv | $app cut -f 1 | md5sum | cut -d " " -f 1)
+
+
+# ----------------------------------------------------------------------------
+# csvtk xxx
+# ----------------------------------------------------------------------------
