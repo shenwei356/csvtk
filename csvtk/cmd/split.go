8eb70952976886f796b0cbfedaaf3246c1b3c28b
Wei Shen <shenwei356@gmail.com>
2019-08-14 22:17:27 +0800

    split: rename variables

--- csvtk/cmd/split.go
+++ csvtk/cmd/split.go
@@ -319,7 +319,7 @@ func init() {
 
 }
 
-var moreThanOneWrite sync.Map
+var writtenFiles sync.Map
 
 func appendRows(config Config,
 	printMetaLine bool,
@@ -327,19 +327,18 @@ func appendRows(config Config,
 	headerRow []string,
 	outFile string,
 	rows [][]string,
-	// moreThanOneWrite map[string]bool,
 	key string,
 ) {
 
 	var outfh *xopen.Writer
 	var err error
 
-	_, writeOnce := moreThanOneWrite.Load(key)
-	if writeOnce {
+	_, written := writtenFiles.Load(key)
+	if written {
 		outfh, err = xopen.WopenFile(outFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	} else {
 		outfh, err = xopen.Wopen(outFile)
-		moreThanOneWrite.Store(key, true)
+		writtenFiles.Store(key, true)
 	}
 	checkError(err)
 	defer outfh.Close()
@@ -354,7 +353,7 @@ func appendRows(config Config,
 	if printMetaLine && len(csvReader.MetaLine) > 0 {
 		outfh.WriteString(fmt.Sprintf("sep=%s\n", string(writer.Comma)))
 	}
-	if !writeOnce && headerRow != nil {
+	if !written && headerRow != nil {
 		checkError(writer.Write(headerRow))
 	}
 
